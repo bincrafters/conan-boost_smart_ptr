efbc5f917546745d459124a754de5ef72b1205d1
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:35 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,16 +3,16 @@ from conans import ConanFile
 
 class BoostSmart_PtrConan(ConanFile):
     name = "Boost.Smart_Ptr"
-    version = "1.65.1"
+    version = "1.66.0"
 
     requires = \
-        "Boost.Config/1.65.1@bincrafters/testing", \
-        "Boost.Assert/1.65.1@bincrafters/testing", \
-        "Boost.Core/1.65.1@bincrafters/testing", \
-        "Boost.Move/1.65.1@bincrafters/testing", \
-        "Boost.Predef/1.65.1@bincrafters/testing", \
-        "Boost.Type_Traits/1.65.1@bincrafters/testing", \
-        "Boost.Throw_Exception/1.65.1@bincrafters/testing"
+        "Boost.Config/1.66.0@bincrafters/testing", \
+        "Boost.Assert/1.66.0@bincrafters/testing", \
+        "Boost.Core/1.66.0@bincrafters/testing", \
+        "Boost.Move/1.66.0@bincrafters/testing", \
+        "Boost.Predef/1.66.0@bincrafters/testing", \
+        "Boost.Type_Traits/1.66.0@bincrafters/testing", \
+        "Boost.Throw_Exception/1.66.0@bincrafters/testing"
  
     lib_short_names = ["smart_ptr"]
     is_header_only = True
@@ -20,9 +20,9 @@ class BoostSmart_PtrConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-smart_ptr"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     exports = "boostgenerator.py"
 
