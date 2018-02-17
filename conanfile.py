bf956cc86d1d63e55316e386d1a23331fd8e2e48
Jerry W <jerrywiltse@gmail.com>
2018-02-17 09:39:04 -0500

    merge from testing

--- conanfile.py
+++ conanfile.py
@@ -17,14 +17,14 @@ class BoostSmart_PtrConan(ConanFile):
         self.info.header_only()
     
     requires = (
-        "boost_package_tools/1.65.1@bincrafters/testing",
-        "boost_config/1.65.1@bincrafters/testing",
-        "boost_assert/1.65.1@bincrafters/testing",
-        "boost_core/1.65.1@bincrafters/testing",
-        "boost_move/1.65.1@bincrafters/testing",
-        "boost_predef/1.65.1@bincrafters/testing",
-        "boost_type_traits/1.65.1@bincrafters/testing",
-        "boost_throw_exception/1.65.1@bincrafters/testing"
+        "boost_package_tools/1.65.1@bincrafters/stable",
+        "boost_config/1.65.1@bincrafters/stable",
+        "boost_assert/1.65.1@bincrafters/stable",
+        "boost_core/1.65.1@bincrafters/stable",
+        "boost_move/1.65.1@bincrafters/stable",
+        "boost_predef/1.65.1@bincrafters/stable",
+        "boost_type_traits/1.65.1@bincrafters/stable",
+        "boost_throw_exception/1.65.1@bincrafters/stable"
     )
 
     # BEGIN
@@ -32,7 +32,7 @@ class BoostSmart_PtrConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_65_1"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.65.1@bincrafters/testing"
+    build_requires = "boost_generator/1.65.1@bincrafters/stable"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
