e84dbf8710c70ece421e358e5b02dc534eedacf9
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:12 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostSmart_PtrConan(base.BoostBaseConan):
     name = "boost_smart_ptr"
@@ -21,5 +21,3 @@ class BoostSmart_PtrConan(base.BoostBaseConan):
         "boost_throw_exception",
         "boost_type_traits"
     ]
-
-
