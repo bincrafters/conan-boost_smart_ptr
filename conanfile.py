47698590caa389554f83ef4a1fb002725cc27143
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:08 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,21 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostSmart_PtrConan(base.BoostBaseConan):
     name = "boost_smart_ptr"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_smart_ptr"
-    lib_short_names = ["smart_ptr"]
-    header_only_libs = ["smart_ptr"]
-    b2_requires = [
-        "boost_assert",
-        "boost_config",
-        "boost_core",
-        "boost_move",
-        "boost_predef",
-        "boost_static_assert",
-        "boost_throw_exception",
-        "boost_type_traits"
-    ]
+    version = "1.70.0"
